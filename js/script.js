d2b1dafadb3fb08060a5cac934aeae2c77fa6889
aoshiro <koh.sss0629@gmail.com>
2021-04-22 23:45:24 +0900

    スティッキーヘッダーとスライダーとTOPへ戻るボタンの追加

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
-/* FAQのアコーディオン
-/*----------------------- */
-$('.q__text').on('click', function () {
-  $(this).toggleClass('active');
-  $(this).next().slideToggle();
-  if ($(this).hasClass('active')) {
-    $(this).children('.qa__flag').html('-')
-  } else {
-    $(this).children('.qa__flag').html('+')
-  }
-});
-
-/* スティッキーヘッダー
-/*----------------------- */
 $(function () {
+  /* FAQのアコーディオン
+  /*----------------------- */
+  $('.q__text').on('click', function () {
+    $(this).toggleClass('active');
+    $(this).next().slideToggle();
+    if ($(this).hasClass('active')) {
+      $(this).children('.qa__flag').html('-')
+    } else {
+      $(this).children('.qa__flag').html('+')
+    }
+  });
+  
+  /* スティッキーヘッダー
+  /*----------------------- */
   var navPos = $('.header__wrapper').offset().top;
   $(window).scroll(function () {
     if ($(window).scrollTop() > navPos) {
-      $(".header__wrapper").css("position", "fixed");
+      $('.header__wrapper').addClass('sticky');
     } else {
-      $(".header__wrapper").css("position", "static");
+      $('.header__wrapper').removeClass('sticky');
+    }
+  });
+
+  $(window).scroll(function () {
+    var now = $(window).scrollTop();    
+    if (now > $('.mv__bg').height()) {
+      $('#page_top').fadeIn("slow");
+    } else {
+      $('#page_top').fadeOut('slow');
     }
   });
 });
