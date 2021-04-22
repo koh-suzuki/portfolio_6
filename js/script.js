2e58bffc8d03bce4e0f4db237293fdc84bd8685d
aoshiro <koh.sss0629@gmail.com>
2021-04-22 20:06:59 +0900

    stickyヘッダー追加

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-$('.q__text').on('click', function(){
+/* FAQのアコーディオン
+/*----------------------- */
+$('.q__text').on('click', function () {
   $(this).toggleClass('active');
   $(this).next().slideToggle();
-  if($(this).hasClass('active')) {
+  if ($(this).hasClass('active')) {
     $(this).children('.qa__flag').html('-')
   } else {
     $(this).children('.qa__flag').html('+')
   }
+});
+
+/* スティッキーヘッダー
+/*----------------------- */
+$(function () {
+  var navPos = $('.header__wrapper').offset().top;
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > navPos) {
+      $(".header__wrapper").css("position", "fixed");
+    } else {
+      $(".header__wrapper").css("position", "static");
+    }
+  });
 });
