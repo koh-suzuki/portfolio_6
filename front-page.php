6b9688b420709a9a2690995987cf563a607e3a46
aoshiro <koh.sss0629@gmail.com>
2021-05-06 17:44:00 +0900

    .wrap で右側の余白をけしました。

--- front-page.php
+++ front-page.php
@@ -6,38 +6,40 @@
 </head>
 
 <body id="header">
-  <header class="header__wrapper">
-    <?php get_template_part('includes/header'); ?>
-  </header>
-  <main class="main">
-    <!-- main view -->
-    <?php get_template_part('includes/main'); ?>
-    <!-- service  -->
-    <!-- service end -->
-    <?php get_template_part('includes/service'); ?>
-    <!-- work -->
-    <?php get_template_part('includes/work'); ?>
-    <!-- work end -->
-    <!-- FAQ -->
-    <?php get_template_part('includes/faq'); ?>
-    <!-- FAQ end -->
-    <!-- INFO -->
-    <?php get_template_part('includes/info'); ?>
-    <!-- INFO end -->
-    <!-- CONTACT -->
-    <?php get_template_part('includes/contact'); ?>
-    <!-- CONTACT end -->
-    <div id="page_top"><a href="#header"></a></div>
-  </main>
-  <footer class="footer">
-    <p class="footer__logo">AOSHIRO TECH</p>
-    <p class="copyright">
-      <small>Ⓒ 2021 AOSHIRO TECH All rights reserved</small>
-    </p>
-  </footer>
-  <!-- includes/footer -->
-  <?php get_template_part('includes/footer'); ?>
-  <!-- includes/footer end -->
+  <div class="wrap">
+    <header class="header__wrapper">
+      <?php get_template_part('includes/header'); ?>
+    </header>
+    <main class="main">
+      <!-- main view -->
+      <?php get_template_part('includes/main'); ?>
+      <!-- service  -->
+      <!-- service end -->
+      <?php get_template_part('includes/service'); ?>
+      <!-- work -->
+      <?php get_template_part('includes/work'); ?>
+      <!-- work end -->
+      <!-- FAQ -->
+      <?php get_template_part('includes/faq'); ?>
+      <!-- FAQ end -->
+      <!-- INFO -->
+      <?php get_template_part('includes/info'); ?>
+      <!-- INFO end -->
+      <!-- CONTACT -->
+      <?php get_template_part('includes/contact'); ?>
+      <!-- CONTACT end -->
+      <div id="page_top"><a href="#header"></a></div>
+    </main>
+    <footer class="footer">
+      <p class="footer__logo">AOSHIRO TECH</p>
+      <p class="copyright">
+        <small>Ⓒ 2021 AOSHIRO TECH All rights reserved</small>
+      </p>
+    </footer>
+    <!-- includes/footer -->
+    <?php get_template_part('includes/footer'); ?>
+    <!-- includes/footer end -->
+  </div>
   <?php get_footer(); ?>
 </body>
 
--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,10 @@
 <div class="header__inner">
   <h1 class="header__logo"><a href="<?php echo esc_url(home_url('/')); ?>">AOSHIRO TECH</a></h1>
-  <nav class="header__nav">
-    <ul class="header__list">
-      <li class="header__item"><a href="<?php echo esc_url(home_url('/')); ?>">トップ</a></li>
-      <li class="header__item"><a href="<?php echo esc_url(home_url('/service')); ?>">学習コース</a></li>
-      <li class="header__item"><a href="<?php echo esc_url(home_url('/postlist')); ?>">お知らせ</a></li>
-      <li class="header__item"><a href="<?php echo esc_url(home_url('/contact')); ?>">お問い合せ</a></li>
-    </ul>
-  </nav>
+  <?php
+  wp_nav_menu(array(
+    'theme_location' => 'main-menu'
+  ));
+  ?>
   <button type="button" id="js-buttonHamburger" class="c-button p-hamburger" aria-controls="global-nav" aria-expanded="false">
     <span class="p-hamburger__line">
       <span class="u-visuallyHidden">
